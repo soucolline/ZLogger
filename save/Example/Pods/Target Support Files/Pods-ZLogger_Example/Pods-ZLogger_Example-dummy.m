0f7d3d98c00623518c8d642331426ac8862c55d2
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2019-07-07 16:18:33 +0200

    chore(swiftkit): move files to save folder

---
+++ save/Example/Pods/Target Support Files/Pods-ZLogger_Example/Pods-ZLogger_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZLogger_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ZLogger_Example
+@end
---
+++ save/Example/Pods/Target Support Files/ZLogger/ZLogger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZLogger : NSObject
+@end
+@implementation PodsDummy_ZLogger
+@end
